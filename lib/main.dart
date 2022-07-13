93225e668290426ea8474a8d167038b2e079b674
Stanislav Leonchik <leonstas1@mail.ru>
2022-07-13 12:05:06 +0300

    Обновил AppBar Добавил изменение иконки лайка

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leadingWidth: 150,
-        leading: Row(
-            children: [
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage()
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  var isLiked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leadingWidth: 150,
+            leading: Row(
+                children: [
+                  IconButton(
+                    icon: new Icon(Icons.keyboard_backspace,
+                    ),
+                    color: Colors.green,
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    "Назад",
+                    style: TextStyle(color: Colors.green, fontSize: 18),
+                  )
+                ],
+            ),
+            actions: [
               IconButton(
-                icon: new Icon(Icons.keyboard_backspace,
+                onPressed: ()
+                {
+                  setState(() {isLiked = !isLiked;});
+                },
+                icon: isLiked
+                  ? const Icon(Icons.favorite)
+                  : const Icon(Icons.favorite_border),
+                color: isLiked
+                  ? Colors.red
+                  : Colors.green
+              ),
+              IconButton(
+                icon: const Icon(Icons.file_upload_outlined,
                 ),
                 color: Colors.green,
                 onPressed: () {},
-              ),
-              const Text(
-                "Назад",
-                style: TextStyle(color: Colors.green, fontSize: 18),
               )
             ],
-        ),
-      )
-  ),
-)
-);
+          ),
+          body: SingleChildScrollView(
+
+          ),
+      );
+  }
+}
